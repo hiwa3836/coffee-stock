ebeb4d3bf3c1b44d824ec881a6af70e8a21371d4
HIWA <yo9439@gmail.com>
2026-07-15 13:07:37 +0900

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,5 @@
+window.CONFIG = {
+    SUPABASE_URL: 'https://dhwdwbhfgoupnxseansd.supabase.co',
+    SUPABASE_KEY: 'sb_publishable_dZl-kSDklZZvEjKO25iV5Q_2kwyyRxI',
+    DISCORD_RELAY_URL: 'https://rcs-stock.yo9439.workers.dev'
+};
