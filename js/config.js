1ee004d9895d4c32860c6f0a5411d2418b3a7b78
HIWA <yo9439@gmail.com>
2026-07-16 00:59:48 +0900

    Add ACCESS_CODE to configuration
    Added ACCESS_CODE for internal sharing.

--- js/config.js
+++ js/config.js
@@ -1,5 +1,7 @@
 window.CONFIG = {
     SUPABASE_URL: 'https://dhwdwbhfgoupnxseansd.supabase.co',
     SUPABASE_KEY: 'sb_publishable_dZl-kSDklZZvEjKO25iV5Q_2kwyyRxI',
-    DISCORD_RELAY_URL: 'https://rcs-stock.yo9439.workers.dev'
+    DISCORD_RELAY_URL: 'https://rcs-stock.yo9439.workers.dev',
+    // 간부끼리만 공유하는 4자리 이상 코드로 바꿔서 사용 (진짜 보안 아님, URL 우연히 접근 막는 용도)
+    ACCESS_CODE: '3836'
 };
